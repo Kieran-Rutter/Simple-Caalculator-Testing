49a67f55b316819ce9057d27623c17672678db30
kieran Rutter <kieranrutter2002@outlook.com>
2023-05-06 11:49:21 +0100

    Basic calculator with pytests test

---
+++ calculator.py
@@ -0,0 +1,73 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit
+
+class Calculator(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Calculator")
+        self.setFixedSize(300, 200)
+        self.initUI()
+
+    def initUI(self):
+        centralWidget = QWidget(self)
+        self.setCentralWidget(centralWidget)
+
+        vbox = QVBoxLayout()
+        hbox = QHBoxLayout()
+
+        self.display = QLineEdit(self)
+        self.display.setFixedHeight(35)
+        vbox.addWidget(self.display)
+
+        hbox.addWidget(self.createButton('7'))
+        hbox.addWidget(self.createButton('8'))
+        hbox.addWidget(self.createButton('9'))
+        hbox.addWidget(self.createButton('/'))
+        vbox.addLayout(hbox)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.createButton('4'))
+        hbox.addWidget(self.createButton('5'))
+        hbox.addWidget(self.createButton('6'))
+        hbox.addWidget(self.createButton('*'))
+        vbox.addLayout(hbox)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.createButton('1'))
+        hbox.addWidget(self.createButton('2'))
+        hbox.addWidget(self.createButton('3'))
+        hbox.addWidget(self.createButton('-'))
+        vbox.addLayout(hbox)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.createButton('0'))
+        hbox.addWidget(self.createButton('.'))
+        hbox.addWidget(self.createButton('='))
+        hbox.addWidget(self.createButton('+'))
+        vbox.addLayout(hbox)
+
+        centralWidget.setLayout(vbox)
+
+    def createButton(self, text):
+        button = QPushButton(text)
+        button.setFixedSize(60, 40)
+        button.clicked.connect(lambda: self.buttonClicked(text))
+        return button
+
+    def buttonClicked(self, text):
+        if text == '=':
+            try:
+                result = str(eval(self.display.text()))
+            except:
+                result = 'Error'
+            self.display.setText(result)
+        elif text == 'C':
+            self.display.setText('')
+        else:
+            self.display.setText(self.display.text() + text)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    calc = Calculator()
+    calc.show()
+    sys.exit(app.exec_())
---
+++ test_calc.py
@@ -0,0 +1,51 @@
+import pytest
+from PyQt5.QtWidgets import QApplication
+from calculator import Calculator
+
+def test_addition():
+    app = QApplication([])
+    calc = Calculator()
+    calc.display.setText('2+2')
+    calc.buttonClicked('=')
+    assert calc.display.text() == '4'
+    app.exit()
+
+def test_subtraction():
+    app = QApplication([])
+    calc = Calculator()
+    calc.display.setText('10-5')
+    calc.buttonClicked('=')
+    assert calc.display.text() == '5'
+    app.exit()
+
+def test_multiplication():
+    app = QApplication([])
+    calc = Calculator()
+    calc.display.setText('3*4')
+    calc.buttonClicked('=')
+    assert calc.display.text() == '12'
+    app.exit()
+
+def test_division():
+    app = QApplication([])
+    calc = Calculator()
+    calc.display.setText('10/2')
+    calc.buttonClicked('=')
+    assert calc.display.text() == '5.0'
+    app.exit()
+
+def test_division_by_zero():
+    app = QApplication([])
+    calc = Calculator()
+    calc.display.setText('5/0')
+    calc.buttonClicked('=')
+    assert calc.display.text() == 'Error'
+    app.exit()
+
+def test_clear_display():
+    app = QApplication([])
+    calc = Calculator()
+    calc.display.setText('3+4')
+    calc.buttonClicked('C')
+    assert calc.display.text() == ''
+    app.exit()
